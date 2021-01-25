d7ac708aa429a09bbcf7535774ab01023016fe8f
Allan Nielsen <Allann@users.noreply.github.com>
2021-01-25 19:49:09 +1000

    Add initial source code

---
+++ src/Hs.Core.Money/CurrencyRegistry.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hs.Core.Money
+{
+    /// <summary>Represent the central thread-safe registry for currencies.</summary>
+    internal class CurrencyRegistry
+    {
+        private static readonly Dictionary<string, byte> Namespaces;
+        private static readonly Dictionary<string, Currency> Currencies;
+
+        static CurrencyRegistry()
+        {
+            DefaultCurrencies.EnsureCurrencyTable();
+            Namespaces = new Dictionary<string, byte> { ["ISO-4217"] = default, ["ISO-4217-HISTORIC"] = default };
+            var cs = DefaultCurrencies.Currencies ?? new List<Currency>();
+            Currencies = new Dictionary<string, Currency>(cs.ToDictionary(k => k.Namespace + "::" + k.Code));
+        }
+
+        /// <summary>Tries the get <see cref="Currency"/> of the given code and namespace.</summary>
+        /// <param name="code">A currency code, like EUR or USD.</param>
+        /// <param name="currency">When this method returns, contains the <see cref="Currency"/> that has the specified code, or the default value of the type if the operation failed.</param>
+        /// <returns><b>true</b> if <see cref="CurrencyRegistry"/> contains a <see cref="Currency"/> with the specified code; otherwise, <b>false</b>.</returns>
+        /// <exception cref="ArgumentNullException">The value of 'code' cannot be null or empty.</exception>
+        public bool TryGet(string code, out Currency currency)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentNullException(nameof(code));
+            }
+
+            var items = Namespaces.Keys.Select(ns => Currencies.TryGetValue(ns + "::" + code, out var c));
+            var found = new List<Currency>();
+            foreach (var ns in Namespaces.Keys)
+            {
+                // don't use string.Format(), string concat much faster in this case!
+                if (Currencies.TryGetValue(ns + "::" + code, out var c))
+                {
+                    found.Add(c);
+                }
+            }
+
+            currency = found.FirstOrDefault(); // TODO: If more than one, sort by prio.
+            return !currency.Equals(default);
+        }
+
+        /// <summary>Tries the get <see cref="Currency"/> of the given code and namespace.</summary>
+        /// <param name="code">A currency code, like EUR or USD.</param>
+        /// <param name="namespace">A namespace, like ISO-4217.</param>
+        /// <param name="currency">When this method returns, contains the <see cref="Currency"/> that has the specified code and namespace, or the default value of the type if the operation failed.</param>
+        /// <returns><b>true</b> if <see cref="CurrencyRegistry"/> contains a <see cref="Currency"/> with the specified code; otherwise, <b>false</b>.</returns>
+        /// <exception cref="ArgumentNullException">The value of 'code' or 'namespace' cannot be null or empty.</exception>
+        public bool TryGet(string code, string @namespace, out Currency currency)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentNullException(nameof(code));
+            }
+
+            if (string.IsNullOrWhiteSpace(@namespace))
+            {
+                throw new ArgumentNullException(nameof(@namespace));
+            }
+
+            return Currencies.TryGetValue(@namespace + "::" + code, out currency); // don't use string.Format(), string concat much faster in this case!
+        }
+
+        /// <summary>Attempts to add the <see cref="Currency"/> of the given code and namespace.</summary>
+        /// <param name="code">A currency code, like EUR or USD.</param>
+        /// <param name="namespace">A namespace, like ISO-4217.</param>
+        /// <param name="currency">When this method returns, contains the <see cref="Currency"/> that has the specified code and namespace, or the default value of the type if the operation failed.</param>
+        /// <returns><b>true</b> if the <see cref="Currency"/> with the specified code is added; otherwise, <b>false</b>.</returns>
+        /// <exception cref="ArgumentNullException">The value of 'code' or 'namespace' cannot be null or empty.</exception>
+        public bool TryAdd(string code, string @namespace, Currency currency)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentNullException(nameof(code));
+            }
+
+            if (string.IsNullOrWhiteSpace(@namespace))
+            {
+                throw new ArgumentNullException(nameof(@namespace));
+            }
+
+            Namespaces[@namespace] = default;
+            var key = @namespace + "::" + code;
+
+            if (Currencies.ContainsKey(key))
+            {
+                return false;
+            }
+
+            Currencies.Add(key, currency);
+            return true;
+        }
+
+        /// <summary>Attempts to remove the <see cref="Currency"/> of the given code and namespace.</summary>
+        /// <param name="code">A currency code, like EUR or USD.</param>
+        /// <param name="namespace">A namespace, like ISO-4217.</param>
+        /// <param name="currency">When this method returns, contains the <see cref="Currency"/> that has the specified code and namespace, or the default value of the type if the operation failed.</param>
+        /// <returns><b>true</b> if the <see cref="Currency"/> with the specified code is removed; otherwise, <b>false</b>.</returns>
+        /// <exception cref="ArgumentNullException">The value of 'code' or 'namespace' cannot be null or empty.</exception>
+        public bool TryRemove(string code, string @namespace, out Currency currency)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentNullException(nameof(code));
+            }
+
+            if (string.IsNullOrWhiteSpace(@namespace))
+            {
+                throw new ArgumentNullException(nameof(@namespace));
+            }
+
+            var key = @namespace + "::" + code;
+            if (Currencies.ContainsKey(key))
+            {
+                currency = Currencies[key];
+                Currencies.Remove(key);
+                if (!Currencies.Any(c => c.Value.Namespace == @namespace))
+                {
+                    Namespaces.Remove(@namespace);
+                }
+
+                return true;
+            }
+            else
+            {
+                currency = new Currency();
+                return false;
+            }
+        }
+
+        /// <summary>Get all registered currencies.</summary>
+        /// <returns>An <see cref="IEnumerable{Currency}"/> of all registered currencies.</returns>
+        public IEnumerable<Currency> GetAllCurrencies()
+        {
+            DefaultCurrencies.EnsureCurrencyTable();
+            return DefaultCurrencies.Currencies ?? new List<Currency>();
+        }
+    }
+}
---
+++ tests/Hs.Core.Money.Tests/Serialization/JsonSerializableSpec.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Hs.Core.Money.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Hs.Core.Money.Serialization.Tests
+{
+    public static class JsonSerializableSpec
+    {
+        public class GivenIWantToSerializeAmounWithJsonSerializer
+        {
+            public static IEnumerable<object[]> TestData => new[]
+            {
+                new object[] { new Amount(765m, Currency.FromCode("JPY")) },
+                new object[] { new Amount(765.43m, Currency.FromCode("EUR")) }
+            };
+
+            [Theory]
+            [MemberData(nameof(TestData))]
+            public void WhenSerializing_ThenThisShouldSucceed(Amount money)
+            {
+                var json = money.ToJson();
+                var clone = json.FromJson<Amount>();
+
+                clone.Should().Be(money);
+            }
+        }
+
+        public class GivenIWantToDeserializeAmountWithJsonSerializer
+        {
+            private static string CurrentCultureCode = new RegionInfo(CultureInfo.CurrentCulture.LCID).ISOCurrencySymbol;
+
+            public static IEnumerable<object[]> ValidJsonData => new[]
+            {
+                new object[] { $"{{ \"value\": 200, \"currency\": \"{CurrentCultureCode}\" }}" },
+                new object[] { $"{{ \"currency\": \"{CurrentCultureCode}\", \"value\": 200 }}" },
+            };
+
+            public static IEnumerable<object[]> InvalidJsonData => new[]
+            {
+                new object[] { "{ value: '200' }" },
+                new object[] { "{ value: 200 }" },
+            };
+
+            public static IEnumerable<object[]> ValidNestedJsonData => new[]
+            {
+                new object[] { $"{{ cash: {{ value: '200', currency: '{CurrentCultureCode}' }} }}", },
+                new object[] { $"{{ cash: {{ value: 200, currency: '{CurrentCultureCode}' }} }}" },
+                new object[] { $"{{ cash: {{ currency: '{CurrentCultureCode}', value: 200 }} }}" },
+                new object[] { $"{{ cash: {{ currency: '{CurrentCultureCode}', value: '200' }} }}" }
+            };
+
+            public static IEnumerable<object[]> ValidNestedNullableJsonData => new[]
+            {
+                new object[] { $"{{ cash: {{ value: '200', currency: '{CurrentCultureCode}' }} }}", },
+                new object[] { $"{{ cash: {{ value: 200, currency: '{CurrentCultureCode}' }} }}" },
+                new object[] { $"{{ cash: {{ currency: '{CurrentCultureCode}', value: 200 }} }}" },
+                new object[] { $"{{ cash: {{ currency: '{CurrentCultureCode}', value: '200' }} }}" },
+                new object[] { $"{{ cash: null }}" },
+            };
+
+            [Theory]
+            [MemberData(nameof(ValidJsonData))]
+            public void WhenDeserializingWithValidJson_ThenThisShouldSucceed(string json)
+            {
+                var money = new Amount(200, Currency.FromCode(CurrentCultureCode));
+
+                var clone = json.FromJson<Amount>();
+
+                clone.Should().Be(money);
+            }
+
+            [Theory]
+            [MemberData(nameof(InvalidJsonData))]
+            public void WhenDeserializingWithInvalidJson_ThenThisShouldFail(string json)
+            {
+                Action a = () => json.FromJson<Amount>();
+                a.Should().Throw<ArgumentNullException>();
+            }
+
+            private class TypeWithMoneyProperty
+            {
+                public Amount Cash { get; set; }
+            }
+
+            [Theory]
+            [MemberData(nameof(ValidNestedJsonData))]
+            public void WhenDeserializingWithNested_ThenThisShouldSucceed(string json)
+            {
+                var money = new Amount(200, Currency.FromCode(CurrentCultureCode));
+
+                var clone = json.FromJson<TypeWithMoneyProperty>();
+
+                clone.Cash.Should().Be(money);
+            }
+
+            private class TypeWithNullableMoneyProperty
+            {
+                public Amount? Cash { get; set; }
+            }
+
+            [Theory]
+            [MemberData(nameof(ValidNestedNullableJsonData))]
+            public void WhenDeserializingWithNestedNullable_ThenThisShouldSucceed(string json)
+            {
+                var money = new Amount(200, Currency.FromCode(CurrentCultureCode));
+
+                var clone = json.FromJson<TypeWithNullableMoneyProperty>();
+
+                if (!json.Contains("null"))
+                    clone.Cash.Should().Be(money);
+                else
+                    clone.Cash.Should().BeNull();
+            }
+        }
+    }
+}
